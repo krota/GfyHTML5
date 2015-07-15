69011f16ebd57837d1edbbc794a58fe5a21585b6
Kyle Rota <Kyle.Rota@gmail.com>
2015-07-14 18:29:11 -0700

    Removed logging

--- background.js
+++ background.js
@@ -1,5 +1,4 @@
 chrome.webNavigation.onBeforeNavigate.addListener(function (details) {
-  console.log('onBeforeNavigate ', details.url);
   if (details && details.url && details.url.match('(https?:\/\/)[a-z]+[\.]+(gfycat.com)[/]')) {
   	var gfyPath = details.url.split('/').pop().split('.').shift();
     chrome.tabs.update(details.tabId, {url: 'https://gfycat.com/' + gfyPath});
